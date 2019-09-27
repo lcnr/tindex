5d94a3951ca9c858ef3870d620cbae2372f1a193
lcnr/Bastian Kauschke <bastian_kauschke@hotmail.de>
2019-09-27 13:03:35 +0200

    Initial commit

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,16 @@
 use std::{
+    fmt,
     iter::FromIterator,
     marker::PhantomData,
     ops::{Deref, DerefMut, Index, IndexMut},
     slice::{Iter, IterMut},
     vec::IntoIter,
-    fmt,
+    cmp::{PartialEq, Eq},
 };
+
+pub mod slice_index;
+
+use slice_index::TSliceIndex;
 
 pub trait TIndex: From<usize> {
     fn as_index(self) -> usize;
@@ -23,7 +28,39 @@ impl<I, T: fmt::Debug> fmt::Debug for TSlice<I, T> {
     }
 }
 
+impl<I, T: PartialEq> PartialEq for TSlice<I, T> {
+    fn eq(&self, other: &Self) -> bool {
+        if self.len() == other.len() {
+            self.iter().zip(other.iter()).all(|(s, o)| s == o)
+        } else {
+            false
+        }
+    }
+}
+
+impl<I, T: Eq> Eq for TSlice<I, T> { }
+
 impl<I, T> TSlice<I, T> {
+    pub fn len(&self) -> usize {
+        self.inner.len()
+    }
+
+    pub fn first(&self) -> Option<&T> {
+        self.inner.first()
+    }
+
+    pub fn first_mut(&mut self) -> Option<&mut T> {
+        self.inner.first_mut()
+    }
+
+    pub fn last(&self) -> Option<&T> {
+        self.inner.last()
+    }
+
+    pub fn split_last(&self) -> Option<(&T, &Self)> {
+        self.inner.split_last().map(|(t, slice)| (t, slice.into()))
+    }
+
     pub fn iter<'a>(&'a self) -> Iter<'a, T> {
         self.inner.iter()
     }
@@ -33,17 +70,55 @@ impl<I, T> TSlice<I, T> {
     }
 }
 
-impl<I: TIndex, T> Index<I> for TSlice<I, T> {
-    type Output = T;
+impl<I: TIndex, T> TSlice<I, T> {
+    pub fn split_at(&self, mid: I) -> (&Self, &Self) {
+        let (left, right) = self.inner.split_at(mid.as_index());
+        (left.into(), right.into())
+    }
 
-    fn index(&self, index: I) -> &T {
-        &self.inner[index.as_index()]
+    pub fn split_at_mut(&mut self, mid: I) -> (&mut Self, &mut Self) {
+        let (left, right) = self.inner.split_at_mut(mid.as_index());
+        (left.into(), right.into())
     }
 }
 
-impl<I: TIndex, T> IndexMut<I> for TSlice<I, T> {
-    fn index_mut(&mut self, index: I) -> &mut T {
-        &mut self.inner[index.as_index()]
+impl<'a, I, T> From<&'a [T]> for &'a TSlice<I, T> {
+    #[inline]
+    fn from(slice: &'a [T]) -> Self {
+        unsafe {
+            // SAFETY: as `TSlice` is `#[repr(transparent)]`
+            // casting from `[T]` to `TSlice<I, T>` is safe
+            let ptr = slice as *const _;
+            let cast = ptr as *const TSlice<I, T>;
+            &*cast
+        }
+    }
+}
+
+impl<'a, I, T> From<&'a mut [T]> for &'a mut TSlice<I, T> {
+    #[inline]
+    fn from(slice: &'a mut [T]) -> Self {
+        unsafe {
+            // SAFETY: as `TSlice` is `#[repr(transparent)]`
+            // casting from `[T]` to `TSlice<I, T>` is safe
+            let ptr = slice as *mut _;
+            let cast = ptr as *mut TSlice<I, T>;
+            &mut *cast
+        }
+    }
+}
+
+impl<I: TIndex, S: TSliceIndex<TSlice<I, T>>, T> Index<S> for TSlice<I, T> {
+    type Output = S::Output;
+
+    fn index(&self, index: S) -> &S::Output {
+        index.index(self)
+    }
+}
+
+impl<I: TIndex, S: TSliceIndex<TSlice<I, T>>, T> IndexMut<S> for TSlice<I, T> {
+    fn index_mut(&mut self, index: S) -> &mut S::Output {
+        index.index_mut(self)
     }
 }
 
@@ -83,6 +158,14 @@ impl<I, T: Clone> Clone for TVec<I, T> {
     }
 }
 
+impl<I, T: PartialEq> PartialEq for TVec<I, T> {
+    fn eq(&self, other: &Self) -> bool {
+        self == other
+    }
+}
+
+impl<I, T: Eq> Eq for TVec<I, T> { }
+
 impl<I, T> TVec<I, T> {
     pub fn new() -> Self {
         Self {
@@ -101,10 +184,6 @@ impl<I, T> TVec<I, T> {
     pub fn pop(&mut self) -> Option<T> {
         self.inner.pop()
     }
-
-    pub fn last(&self) -> Option<&T> {
-        self.inner.last()
-    }
 }
 
 impl<I: TIndex, T> TVec<I, T> {
@@ -114,12 +193,49 @@ impl<I: TIndex, T> TVec<I, T> {
         idx.into()
     }
 
+    pub fn remove(&mut self, id: I) -> T {
+        self.inner.remove(id.as_index())
+    }
+
     pub fn last_id(&self) -> Option<I> {
         if self.inner.is_empty() {
             None
         } else {
             Some((self.inner.len() - 1).into())
         }
+    }
+}
+
+impl<'a, I, T> From<&'a Vec<T>> for &'a TVec<I, T> {
+    #[inline]
+    fn from(vec: &'a Vec<T>) -> Self {
+        unsafe {
+            // SAFETY: as `TVec` is `#[repr(transparent)]`
+            // casting from `Vec<T>` to `TSlice<I, T>` is safe
+            let ptr = vec as *const _;
+            let cast = ptr as *const TVec<I, T>;
+            &*cast
+        }
+    }
+}
+
+impl<'a, I, T> From<&'a mut Vec<T>> for &'a mut TVec<I, T> {
+    #[inline]
+    fn from(vec: &'a mut Vec<T>) -> Self {
+        unsafe {
+            // SAFETY: as `TVec` is `#[repr(transparent)]`
+            // casting from `Vec<T>` to `TSlice<I, T>` is safe
+            let ptr = vec as *mut _;
+            let cast = ptr as *mut TVec<I, T>;
+            &mut *cast
+        }
+    }
+}
+
+impl<I, T> From<Vec<T>> for TVec<I, T> {
+    #[inline]
+    fn from(vec: Vec<T>) -> Self {
+        Self::from_vec(vec)
     }
 }
 
@@ -164,25 +280,13 @@ impl<I, T> Deref for TVec<I, T> {
     type Target = TSlice<I, T>;
 
     fn deref<'a>(&'a self) -> &'a TSlice<I, T> {
-        unsafe {
-            // SAFETY: as `TSlice` is `#[repr(transparent)]`
-            // casting from `[T]` to `TSlice<I, T>` is safe
-            let ptr = self.inner.deref() as *const _;
-            let cast = ptr as *const TSlice<I, T>;
-            &*cast
-        }
+        self.inner.deref().into()
     }
 }
 
 impl<I, T> DerefMut for TVec<I, T> {
     fn deref_mut<'a>(&'a mut self) -> &'a mut TSlice<I, T> {
-        unsafe {
-            // SAFETY: as `TSlice` is `#[repr(transparent)]`
-            // casting from `[T]` to `TSlice<I, T>` is safe
-            let ptr = self.inner.deref_mut() as *mut _;
-            let cast = ptr as *mut TSlice<I, T>;
-            &mut *cast
-        }
+        self.inner.deref_mut().into()
     }
 }
 
---
+++ src/slice_index.rs
@@ -0,0 +1,220 @@
+use std::ops::{Range, RangeFrom, RangeFull, RangeInclusive, RangeTo, RangeToInclusive};
+
+use crate::{TIndex, TSlice};
+
+mod private {
+    use super::*;
+
+    pub trait Sealed {}
+
+    impl<I: TIndex> Sealed for I { }
+    impl<I: TIndex> Sealed for Range<I> { }
+    impl<I: TIndex> Sealed for RangeFrom<I> { }
+    impl Sealed for RangeFull { }
+    impl<I: TIndex> Sealed for RangeInclusive<I> { }
+    impl<I: TIndex> Sealed for RangeTo<I> { }
+    impl<I: TIndex> Sealed for RangeToInclusive<I> { }
+}
+
+pub trait TSliceIndex<T: ?Sized>: private::Sealed {
+    /// The output type returned by methods.
+    type Output: ?Sized;
+
+    /// Returns a shared reference to the output at this location, if in
+    /// bounds.
+    fn get(self, slice: &T) -> Option<&Self::Output>;
+
+    /// Returns a mutable reference to the output at this location, if in
+    /// bounds.
+    fn get_mut(self, slice: &mut T) -> Option<&mut Self::Output>;
+
+    /// Returns a shared reference to the output at this location, panicking
+    /// if out of bounds.
+    fn index(self, slice: &T) -> &Self::Output;
+
+    /// Returns a mutable reference to the output at this location, panicking
+    /// if out of bounds.
+    fn index_mut(self, slice: &mut T) -> &mut Self::Output;
+}
+
+
+impl<I: TIndex, T> TSliceIndex<TSlice<I, T>> for I {
+    type Output = T;
+
+    fn get(self, slice: &TSlice<I, T>) -> Option<&Self::Output> {
+        slice.inner.get(self.as_index())
+    }
+
+    fn get_mut(self, slice: &mut TSlice<I, T>) -> Option<&mut Self::Output> {
+        slice.inner.get_mut(self.as_index())
+    }
+
+    fn index(self, slice: &TSlice<I, T>) -> &Self::Output {
+        &slice.inner[self.as_index()]
+    }
+
+    fn index_mut(self, slice: &mut TSlice<I, T>) -> &mut Self::Output {
+        &mut slice.inner[self.as_index()]
+    }
+}
+
+impl<I: TIndex, T> TSliceIndex<TSlice<I, T>> for Range<I> {
+    type Output = TSlice<I, T>;
+
+    fn get(self, slice: &TSlice<I, T>) -> Option<&Self::Output> {
+        slice.inner.get(Range {
+            start: self.start.as_index(),
+            end: self.end.as_index()
+        }).map(|s| s.into())
+    }
+
+    fn get_mut(self, slice: &mut TSlice<I, T>) -> Option<&mut Self::Output> {
+        slice.inner.get_mut(Range {
+            start: self.start.as_index(),
+            end: self.end.as_index()
+        }).map(|s| s.into())
+    }
+
+    fn index(self, slice: &TSlice<I, T>) -> &Self::Output {
+        slice.inner[Range {
+            start: self.start.as_index(),
+            end: self.end.as_index()
+        }].into()
+    }
+
+    fn index_mut(self, slice: &mut TSlice<I, T>) -> &mut Self::Output {
+        (&mut slice.inner[Range {
+            start: self.start.as_index(),
+            end: self.end.as_index()
+        }]).into()
+    }
+}
+
+impl<I: TIndex, T> TSliceIndex<TSlice<I, T>> for RangeFrom<I> {
+    type Output = TSlice<I, T>;
+
+    fn get(self, slice: &TSlice<I, T>) -> Option<&Self::Output> {
+        slice.inner.get(RangeFrom {
+            start: self.start.as_index(),
+        }).map(|s| s.into())
+    }
+
+    fn get_mut(self, slice: &mut TSlice<I, T>) -> Option<&mut Self::Output> {
+        slice.inner.get_mut(RangeFrom {
+            start: self.start.as_index(),
+        }).map(|s| s.into())
+    }
+
+    fn index(self, slice: &TSlice<I, T>) -> &Self::Output {
+        slice.inner[RangeFrom {
+            start: self.start.as_index(),
+        }].into()
+    }
+
+    fn index_mut(self, slice: &mut TSlice<I, T>) -> &mut Self::Output {
+        (&mut slice.inner[RangeFrom {
+            start: self.start.as_index(),
+        }]).into()
+    }
+}
+
+impl<I: TIndex, T> TSliceIndex<TSlice<I, T>> for RangeFull {
+    type Output = TSlice<I, T>;
+
+    fn get(self, slice: &TSlice<I, T>) -> Option<&Self::Output> {
+        slice.inner.get(self).map(|s| s.into())
+    }
+
+    fn get_mut(self, slice: &mut TSlice<I, T>) -> Option<&mut Self::Output> {
+        slice.inner.get_mut(self).map(|s| s.into())
+    }
+
+    fn index(self, slice: &TSlice<I, T>) -> &Self::Output {
+        slice.inner[self].into()
+    }
+
+    fn index_mut(self, slice: &mut TSlice<I, T>) -> &mut Self::Output {
+        (&mut slice.inner[self]).into()
+    }
+}
+
+fn range_inclusive<I: TIndex>(range: RangeInclusive<I>) -> RangeInclusive<usize> {
+    let (start, end) = range.into_inner();
+    RangeInclusive::new(start.as_index(), end.as_index())
+}
+
+impl<I: TIndex, T> TSliceIndex<TSlice<I, T>> for RangeInclusive<I> {
+    type Output = TSlice<I, T>;
+
+    fn get(self, slice: &TSlice<I, T>) -> Option<&Self::Output> {
+        slice.inner.get(range_inclusive(self)).map(|s| s.into())
+    }
+
+    fn get_mut(self, slice: &mut TSlice<I, T>) -> Option<&mut Self::Output> {
+        slice.inner.get_mut(range_inclusive(self)).map(|s| s.into())
+    }
+
+    fn index(self, slice: &TSlice<I, T>) -> &Self::Output {
+        slice.inner[range_inclusive(self)].into()
+    }
+
+    fn index_mut(self, slice: &mut TSlice<I, T>) -> &mut Self::Output {
+        (&mut slice.inner[range_inclusive(self)]).into()
+    }
+}
+
+impl<I: TIndex, T> TSliceIndex<TSlice<I, T>> for RangeTo<I> {
+    type Output = TSlice<I, T>;
+
+    fn get(self, slice: &TSlice<I, T>) -> Option<&Self::Output> {
+        slice.inner.get(RangeTo {
+            end: self.end.as_index()
+        }).map(|s| s.into())
+    }
+
+    fn get_mut(self, slice: &mut TSlice<I, T>) -> Option<&mut Self::Output> {
+        slice.inner.get_mut(RangeTo {
+            end: self.end.as_index()
+        }).map(|s| s.into())
+    }
+
+    fn index(self, slice: &TSlice<I, T>) -> &Self::Output {
+        slice.inner[RangeTo {
+            end: self.end.as_index()
+        }].into()
+    }
+
+    fn index_mut(self, slice: &mut TSlice<I, T>) -> &mut Self::Output {
+        (&mut slice.inner[RangeTo {
+            end: self.end.as_index()
+        }]).into()
+    }
+}
+
+impl<I: TIndex, T> TSliceIndex<TSlice<I, T>> for RangeToInclusive<I> {
+    type Output = TSlice<I, T>;
+
+    fn get(self, slice: &TSlice<I, T>) -> Option<&Self::Output> {
+        slice.inner.get(RangeToInclusive {
+            end: self.end.as_index()
+        }).map(|s| s.into())
+    }
+
+    fn get_mut(self, slice: &mut TSlice<I, T>) -> Option<&mut Self::Output> {
+        slice.inner.get_mut(RangeToInclusive {
+            end: self.end.as_index()
+        }).map(|s| s.into())
+    }
+
+    fn index(self, slice: &TSlice<I, T>) -> &Self::Output {
+        slice.inner[RangeToInclusive {
+            end: self.end.as_index()
+        }].into()
+    }
+
+    fn index_mut(self, slice: &mut TSlice<I, T>) -> &mut Self::Output {
+        (&mut slice.inner[RangeToInclusive {
+            end: self.end.as_index()
+        }]).into()
+    }
+}
